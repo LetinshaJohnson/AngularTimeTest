91a33cc37fb3b911d16b6cc5c9fbebc90a4d6142
LetinshaJohnson <80558457+LetinshaJohnson@users.noreply.github.com>
2021-03-14 13:16:10 +0530

    Update auth-data.model.ts

--- src/app/authentication/auth-data.model.ts
+++ src/app/authentication/auth-data.model.ts
@@ -8,27 +8,6 @@ export interface RegisterUser{
     user_email:String;
     user_password:String;
 }
-export interface RequestData {
-    email: string;
-    request:string;
-    authorization: any;
-}
-export interface EmailCheck {
-    request:String;
-    email:String;
-}
-export interface changePassword {
-    password:String;
-    login_id:String;
-    login_email:String;
-    authorization: any;
-}
-export interface changeUsername {
-    username:String;
-    login_id:String;
-    login_email:String;
-    authorization: any;
-}
 export interface UserOne {
     role_id:string;
     login_id: string;
