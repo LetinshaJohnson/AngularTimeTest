18031be81d035fb415a1fcd0033b3a0f1f3583c2
LetinshaJohnson <80558457+LetinshaJohnson@users.noreply.github.com>
2021-03-14 13:17:28 +0530

    Update authentication.service.ts

--- src/app/authentication/authentication.service.ts
+++ src/app/authentication/authentication.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { UserData } from '../data-model/user.model';
-import { AuthData, RegisterUser, RequestData, EmailCheck, changePassword, changeUsername, UserOne } from './auth-data.model';
+import { AuthData, RegisterUser, UserOne } from './auth-data.model';
 import { catchError, mapTo, tap } from 'rxjs/operators';
 import { AuthenticationClass, RootObject } from '../classes/auth';
 import { Token } from '../classes/token';
